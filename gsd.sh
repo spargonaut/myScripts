75c66b0a5b24560c334eafd92c6afd023c90d709
james spargo <james.spargo@nike.com>
2018-02-02 09:31:39 -0800

    add a script for git stash drop.

---
+++ gsd.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+### git stash drop stash@{$1}
+### this command is shorthand for dropping a stash at a given index
+
+if [ "$#" -ne 1 ]; then
+    printf "\n\nIllegal number of arguments.  One integer is expected, representing the stash to be dropped.\n\n"
+    exit 1
+fi
+
+#git stash drop stash@{$1}
