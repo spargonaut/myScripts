5b9c3a53a0018a6f7afec444a525d2a958955e7d
james spargo <james.spargo@nike.com>
2018-02-12 09:50:16 -0800

    Add a shortcut for creating custom_command links

---
+++ ccix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+### A shortcut for running the custom_commands initialize script 
+
+CUSTOM_COMMANDS_DIR=$HOME/.custom_commands
+
+cd $CUSTOM_COMMANDS_DIR
+$CUSTOM_COMMANDS_DIR/initialize.sh -s ~/.myScripts
