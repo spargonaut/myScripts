4c1337d186918661b1a38b2506f3632f9b080c19
james spargo <james.spargo@nike.com>
2018-02-12 09:43:33 -0800

    Add a script for running custom_commands initialize in dry-run mode

---
+++ cci.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+### ~/.custom_commands/initialize.sh -s ~/.myScripts -d
+### a shortcut for running the custom_commands initialize script in dry-run mode
+
+CUSTOM_COMMANDS_DIR=$HOME/.custom_commands
+
+cd $CUSTOM_COMMANDS_DIR
+$CUSTOM_COMMANDS_DIR/initialize.sh -s ~/.myScripts -d
