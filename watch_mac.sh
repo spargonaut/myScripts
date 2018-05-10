dfe50cb8b7f5565e68b0656904f81f134cb3bb23
James Spargo <jspargo@jamasoftware.com>
2018-05-10 09:04:39 -0700

    Add some man page messages to watch_mac

--- watch_mac.sh
+++ watch_mac.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+### This is a _very_ buggy implementation of watch
+### The first argument must be an integer (this is not enforced)
+### The second argument is the command to be watched.  If it has flags, or arguments, you'll need to wrap the whole thing in double quotes.
+
 
 while :; 
   do 
