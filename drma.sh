c0dc50cafaca8b4fa2ead13110e00f1d274a2d63
James Spargo <jspargo@jamasoftware.com>
2018-05-14 11:10:32 -0700

    Update the docker remove container script to be more intuitive

--- drma.sh
+++ drma.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+printf "\n\nDEPRECATED!! 05/14/2018\n"
+printf "It will be deleted on or after 05/14/2019\n"
+printf "This script has been deprecated in favor of drmac.sh\n"
+printf "please update your symlinks\n"
+printf "\n\n"
+
 # stop and remove all docker containers
 
-printf "stopping all containers\n"
-docker stop $(docker ps -aq)
-printf "removing all containers\n"
-docker rm $(docker ps -aq)
+#printf "stopping all containers\n"
+#docker stop $(docker ps -aq)
+#printf "removing all containers\n"
+#docker rm $(docker ps -aq)
