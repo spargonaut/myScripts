c239b2d16c28c88115262272e0f7fa224eae67b8
spargonaut <spargonaut@gmail.com>
2019-12-09 13:10:17 -0800

    add comment and pass args to docker-compose up shortcut

--- dcu.sh
+++ dcu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# shortcut for 'docker-comopse build' followed by 'docker-compose up'
+# any args passed to this shortcut will be appended verbatim to the 'docker-compose up' command
+
 docker-compose build
-docker-compose up
+docker-compose up $@
