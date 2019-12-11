f4289481e13a155f498b09fb6d95840eca6e8c60
spargonaut <spargonaut@gmail.com>
2019-12-11 09:56:14 -0800

    add author email to glo command

--- glo.sh
+++ glo.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-git log --pretty=format:"%C(yellow)%h %C(green)%ad %C(red)%an%Creset %s %C(auto)%d" --graph --date=local
+# shortcut for: 'git log --oneline' with added flags for pretty printint extra information
+
+set -e
+
+git log --pretty=format:"%C(yellow)%h %C(green)%ad %C(red)%an <%C(yellow)%ae%C(red)>%Creset %s %C(auto)%d" --graph --date=local
