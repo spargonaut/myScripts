2a7d921c75a57fd551cee2895c29a68d1ff3211b
james spargo <spargonaut@gmail.com>
2021-04-01 11:17:48 -0700

    fix parameter checking when checking out a stashed file

--- gcos.sh
+++ gcos.sh
@@ -2,7 +2,7 @@
 
 ### shortcut to checkout a whole stash or a single file from a stash
 ### args: 1 - the index of the stash; 2 - the path and filename from the stash to be checked out (optional) 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     printf "\n\nIllegal number of arguments.  One integer is expected, representing the index of stash to show.\n"
     printf "an optional path and filename to checkout a single file\n\n"
     exit 1
