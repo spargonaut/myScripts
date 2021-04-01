041843443c4c4e8df070810144aafbc1615bc64e
james spargo <spargonaut@gmail.com>
2021-04-01 11:27:26 -0700

    update the output in git remote remove origin

--- grro.sh
+++ grro.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 printf "\nYou are about to remove the origin for this repository\n"
+printf "\nCurrent Remotes:\n"
+git remote -v
 
+printf "\n"
 read -r -p "Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     printf "\nRemoving the following origin information:\n"
-    git remote show origin
     git remote remove origin
     printf "\ndone.\n"
 else
     printf "\nok.  leaving the origin as is.\nexiting\n\n"
 fi
 
-
+printf "\n"
