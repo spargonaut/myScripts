b55ddb95894eb40b8c8dda9072a9fddd32a3ad33
james spargo <101730436+spargo-nv@users.noreply.github.com>
2022-04-26 13:19:08 -0700

    print the value of the AWS_PROFILE env var

--- awho.sh
+++ awho.sh
@@ -3,4 +3,6 @@
 # this script depends on the aws cli.  you can find more info here: https://aws.amazon.com/cli/
 # it is used to determine if your credentials are configured and which one credentials you're currently using
 
+printf "\nCurrently using profile: $AWS_PROFILE\n"
+
 aws sts get-caller-identity
